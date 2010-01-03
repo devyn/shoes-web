3ba057b47726e5da1adb199e50b5e8a3ebc54725
Devyn Cairns <devyn.cairns@gmail.com>
2010-01-03 13:31:37 -0800

    why won't it work?!?!?!?!

--- vendor/extensions/disable_heroku_caching/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  after_filter :set_cache_control_header
-  private
-  def set_cache_control_header
-    # disallow any caching
-    headers['Cache-Control'] = 'no-cache'
-  end
-end
